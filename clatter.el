7c7932524e3631b8a1579e3817b3a3c65516e06d
Glenn Thompson <glenn@paren.works>
2026-06-14 15:55:26 +0300

    Release 0.4.0
    Since 0.3.0: SOCKS5/Tor proxy (#30), smart-filter refinements (#31),
    additional WHOIS numerics (#27), buffer cleanup hooks (#26), reaction
    overlay fix (#28), named nick faces, pals/fools lists, and a
    bottom-anchored input line with jump-to-prompt.

--- clatter.el
+++ clatter.el
@@ -4,7 +4,7 @@
 ;; Author: Glenn Thompson
 ;; License: MIT
 ;; URL: https://github.com/parenworks/clatter.el
-;; Version: 0.3.0
+;; Version: 0.4.0
 ;; Package-Requires: ((emacs "30.1"))
 ;; Keywords: comm, irc
 
