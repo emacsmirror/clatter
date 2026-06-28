16db9958b3b608a77bec03c583472c520b1fb269
Glenn Thompson <glenn@paren.works>
2026-06-28 09:44:29 +0300

    Bump version to 0.5.0, update README and GUIDE for merged PRs
    Update contributor attributions for fmqa and trevarj.
    Add new features to README: fool visibility toggle, timestamp side,
    message filling, auth-source PASS, stricter mention matching,
    channel list improvements, clatter-pals.el in file structure.
    Update GUIDE: /toggle-fools command, clatter-timestamp-side and
    clatter-fools-visible config options, /whois defaults to own nick.

--- clatter.el
+++ clatter.el
@@ -4,7 +4,7 @@
 ;; Author: Glenn Thompson <glenn@paren.works>
 ;; SPDX-License-Identifier: MIT
 ;; URL: https://github.com/parenworks/clatter.el
-;; Version: 0.4.3
+;; Version: 0.5.0
 ;; Package-Requires: ((emacs "30.1"))
 ;; Keywords: comm, irc
 
