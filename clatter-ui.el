emacsmirror/clatter

commit cec9fc696f2c7b643a170d4086eda2d1bad01081

ui: Fix broken CTCP ACTION
Regression introduced by 08b576a5805abd516c66de2861f5ba40d4c6dfb9

clatter-ui.el
@@ -500,7 +500,7 @@ non-nil, is the already formatted IRC command to send."
   (let* ((msg-type (or msg-type 'privmsg))
          (buffer (or buffer (current-buffer)))
          (sender (clatter-connection-nick conn)))
-    (clatter-send conn (or line (clatter-irc-privmsg target text)))
+    (clatter-send conn (clatter-irc-privmsg target (or line text)))
     (when (clatter-ui--self-echo-p conn)
       (let* ((nonce (cl-incf clatter--self-echo-nonce))
              (tentative (propertize (copy-sequence text) 'clatter-self-echo-nonce nonce)))

commit 67cfb96d9e58cbd2e637695c779c377c71088a0a

ui: Fix regression in sending replies/reacts
Caused by cec9fc696f2c7b643a170d4086eda2d1bad01081, which was an incomplete fix for a previous regression in CTCP ACTION formatting.

clatter-ui.el
@@ -499,7 +499,9 @@ non-nil, is the already formatted IRC command to send."
   (let* ((msg-type (or msg-type 'privmsg))
          (buffer (or buffer (current-buffer)))
          (sender (clatter-connection-nick conn)))
-    (clatter-send conn (clatter-irc-privmsg target (or line text)))
+    (if (eq 'action msg-type)
+        (clatter-send conn (clatter-irc-privmsg target line))
+      (clatter-send conn (or line (clatter-irc-privmsg target text))))
     (when (clatter-ui--self-echo-p conn)
       (let* ((nonce (cl-incf clatter--self-echo-nonce))
              (tentative (propertize (copy-sequence text) 'clatter-self-echo-nonce nonce)))