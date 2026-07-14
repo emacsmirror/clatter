b7a287304e5647c4c3e1e7e6a5873dd944c9d5f2
F. Magsarian <7354509+fmqa@users.noreply.github.com>
2026-07-14 05:24:36 +0200

    smart: Simplify code
    Safe minor refactoring; Tests pass unchanged.

--- clatter-smart.el
+++ clatter-smart.el
@@ -42,7 +42,7 @@ settings continue to be honored independently."
 (defvar clatter-smart-data nil
   "Smart filter state keyed by nick.
 
-Each value is a list (SIGNAL-COUNT NOISE-COUNT ACTIVE-P).")
+Each value is an improper list (ACTIVE-P SIGNAL-COUNT . NOISE-COUNT).")
 
 (defun clatter-smart-on (buf)
   "Get smart filter data for BUF."
@@ -53,23 +53,18 @@ Each value is a list (SIGNAL-COUNT NOISE-COUNT ACTIVE-P).")
 
 (defun clatter-smart--entry-signal-count (entry)
   "Return signal count from smart state ENTRY."
-  (if (consp entry) (or (car entry) 0) 0))
+  (or (cadr entry) 0))
 
 (defun clatter-smart--entry-noise-count (entry)
   "Return noise count from smart state ENTRY."
-  (cond
-   ((and (consp entry) (consp (cdr entry))) (or (cadr entry) 0))
-   ((consp entry) (or (cdr entry) 0))
-   (t 0)))
+  (or (cddr entry) 0))
 
 (defun clatter-smart--entry-active-p (entry)
   "Return non-nil when smart state ENTRY has observed signal."
-  (and (consp entry)
-       (consp (cdr entry))
-       (nth 2 entry)))
+  (car entry))
 
 (defun clatter-smart-put (buf nick elt)
-  "Record ELT for NICK in BUF and return the SNR value."
+  "Record ELT for NICK in BUF and return an SNR value if NICK is active."
   ;; (stringp elt) => t implies NICK changed to ELT.
   ;; Normalize nicks.
   (setq nick (downcase nick))
@@ -78,31 +73,30 @@ Each value is a list (SIGNAL-COUNT NOISE-COUNT ACTIVE-P).")
   (let* ((is-nick-change (and (stringp nick)
                               (stringp elt)))
          (data (clatter-smart-on buf))
-         (signal-noise (gethash nick data))
-         (signal-count (clatter-smart--entry-signal-count signal-noise))
-         (noise-count (clatter-smart--entry-noise-count signal-noise))
-         (active-p (clatter-smart--entry-active-p signal-noise))
+         (activep-sn (gethash nick data))
+         (signal-count (clatter-smart--entry-signal-count activep-sn))
+         (noise-count (clatter-smart--entry-noise-count activep-sn))
+         (activep (clatter-smart--entry-active-p activep-sn))
          (is-noise (memq (if is-nick-change 'nick elt) clatter-smart-noise)))
     (when is-nick-change
       (remhash nick data)
       (setq nick elt))
     (setq signal-count (+ signal-count (if is-noise 0 1)))
     (setq noise-count (+ noise-count (if is-noise 1 0)))
-    (setq active-p (or active-p (not is-noise)))
-    (puthash nick (list signal-count noise-count active-p) data)
-    ;; Float division: integer division would collapse the ratio to 0 or 1
-    ;; and make `clatter-smart-threshold' meaningless.
-    (if (zerop noise-count)
-        most-positive-fixnum
-      (/ (float signal-count) noise-count))))
+    (setq activep (or activep (not is-noise)))
+    (puthash nick (cons activep (cons signal-count noise-count)) data)
+    (and activep
+         ;; Float division: integer division would collapse the ratio to 0 or 1
+         ;; and make `clatter-smart-threshold' meaningless.
+         (if (zerop noise-count)
+             most-positive-fixnum
+           (/ (float signal-count) noise-count)))))
 
 (defun clatter-smart-eval (buf nick elt)
   "Record ELT for NICK in BUF and return whether NICK is noisy."
-  (let* ((ratio (clatter-smart-put buf nick elt))
-         (key (downcase (if (stringp elt) elt nick)))
-         (entry (gethash key (clatter-smart-on buf))))
-    (or (not (clatter-smart--entry-active-p entry))
-        (< ratio clatter-smart-threshold))))
+  (let ((ratio-if-activep (clatter-smart-put buf nick elt)))
+    (or (not ratio-if-activep)
+        (< ratio-if-activep clatter-smart-threshold))))
 
 (provide 'clatter-smart)
 
